342b00057a29560da0ab2afa17059606fe105ffe
Jinha Chung <jinha.chung@kaist.ac.kr>
2022-11-22 13:49:04 +0900

    L4E12 - pgfault

--- lib/fork.c
+++ lib/fork.c
@@ -5,7 +5,7 @@
 
 // PTE_COW marks copy-on-write page table entries.
 // It is one of the bits explicitly allocated to user processes (PTE_AVAIL).
-#define PTE_COW		0x800
+#define PTE_COW     0x800
 
 //
 // Custom page fault handler - if faulting page is copy-on-write,
@@ -14,28 +14,44 @@
 static void
 pgfault(struct UTrapframe *utf)
 {
-	void *addr = (void *) utf->utf_fault_va;
-	uint32_t err = utf->utf_err;
-	int r;
+    void *addr = (void *) utf->utf_fault_va;
+    uint32_t err = utf->utf_err;
+    int r;
 
-	// Check that the faulting access was (1) a write, and (2) to a
-	// copy-on-write page.  If not, panic.
-	// Hint:
-	//   Use the read-only page table mappings at uvpt
-	//   (see <inc/memlayout.h>).
+    // Check that the faulting access was (1) a write, and (2) to a
+    // copy-on-write page.  If not, panic.
+    // Hint:
+    //   Use the read-only page table mappings at uvpt
+    //   (see <inc/memlayout.h>).
 
-	// LAB 4: Your code here.
+    // LAB 4: Your code here.
+    // look up PTE by referencing UVPT
+    pte_t pte = uvpt[(uint64_t)addr / PGSIZE];
+    // check if CoW page
+    if (!(pte & PTE_COW))
+        panic("pgfault is not CoW\n");
+    // TODO: check if write
+    if (0)
+        panic("pgfault is not a write\n");
 
-	// Allocate a new page, map it at a temporary location (PFTEMP),
-	// copy the data from the old page to the new page, then move the new
-	// page to the old page's address.
-	// Hint:
-	//   You should make three system calls.
-	//   No need to explicitly delete the old page's mapping.
+    // Allocate a new page, map it at a temporary location (PFTEMP),
+    // copy the data from the old page to the new page, then move the new
+    // page to the old page's address.
+    // Hint:
+    //   You should make three system calls.
+    //   No need to explicitly delete the old page's mapping.
 
-	// LAB 4: Your code here.
-
-	panic("pgfault not implemented");
+    // LAB 4: Your code here.
+    // allocate new page, perm: PTE_W and PTE_U --> PTE_USER for syscalls
+    // use temporary location PFTEMP
+    if (sys_page_alloc(0, (void *)PFTEMP, PTE_USER) < 0)
+        panic("sys_page_alloc failed\n");
+    // copy data from old page to new page
+    memcpy((void *)PFTEMP, (const void *)(ROUNDDOWN(addr, PGSIZE)), (size_t)PGSIZE);
+    // move new page to the old page's address
+    if (sys_page_map(0, (void *)PFTEMP, 0, (void *)ROUNDDOWN(addr, PGSIZE), PTE_USER) < 0)
+        panic("sys_page_map failed\n");
+    // TODO: remove temporary location with 'third' syscall?
 }
 
 //
@@ -52,11 +68,11 @@ pgfault(struct UTrapframe *utf)
 static int
 duppage(envid_t envid, unsigned pn)
 {
-	int r;
+    int r;
 
-	// LAB 4: Your code here.
-	panic("duppage not implemented");
-	return 0;
+    // LAB 4: Your code here.
+    panic("duppage not implemented");
+    return 0;
 }
 
 //
@@ -78,14 +94,14 @@ duppage(envid_t envid, unsigned pn)
 envid_t
 fork(void)
 {
-	// LAB 4: Your code here.
-	panic("fork not implemented");
+    // LAB 4: Your code here.
+    panic("fork not implemented");
 }
 
 // Challenge!
 int
 sfork(void)
 {
-	panic("sfork not implemented");
-	return -E_INVAL;
+    panic("sfork not implemented");
+    return -E_INVAL;
 }
